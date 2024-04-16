95709707ddf03a1168cef2ea94737718966c9d9f
nom <vlad.nomerov@yandex.ru>
2024-04-16 22:55:28 +0300

    Точка входа в приложение реализована.

--- src/main/java/org/example/MoodAnalyzer.java
+++ src/main/java/org/example/MoodAnalyzer.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.io.IOException;
+
 public class MoodAnalyzer {
     public static void main(String[] args) {
+        String filePath = "C:\\Users\\monst\\Desktop\\Пример текста.txt";
+        MoodWordList wordList = new MoodWordList();
+        wordList.loadDefaultWords(); // Загружаем стандартные слова
 
+        try {
+            String content = FileReader.readFromFile(filePath);
+            int moodScore = MoodScorer.calculateMoodScore(
+                    content,
+                    wordList.getPositiveWords(),
+                    wordList.getNegativeWords()
+            );
+            System.out.println("Mood Score: " + moodScore);
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
     }
 }
